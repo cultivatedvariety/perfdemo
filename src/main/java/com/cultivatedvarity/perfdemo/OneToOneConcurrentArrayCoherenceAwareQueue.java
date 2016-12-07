e341633c4f4fa650a405e232a7a538b5005dd9d8
cultivated variety <cultivated variety>
2016-12-07 16:23:21 +0000

    initial commit

---
+++ src/main/java/com/cultivatedvarity/perfdemo/OneToOneConcurrentArrayCoherenceAwareQueue.java
@@ -0,0 +1,171 @@
+package com.cultivatedvarity.perfdemo;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+/**
+ * Created by robch on 07/12/2016.
+ */
+public class OneToOneConcurrentArrayCoherenceAwareQueue<E> implements Queue<E> {
+
+    private final E[] buffer;
+    private final int mask;
+
+    private volatile long tail = 0;
+    private long headCache = 0;
+    private volatile long head = 0;
+    private long tailCache = 0;
+
+
+    @SuppressWarnings("unchecked")
+    public OneToOneConcurrentArrayCoherenceAwareQueue(final int size) {
+        buffer = (E[]) new Object[size];
+        mask = size - 1;
+    }
+
+    public boolean add(final E e) {
+        if (offer(e)) {
+            return true;
+        }
+
+        throw new IllegalStateException("Queue is full");
+    }
+
+    public boolean offer(final E e) {
+        //in a queue, append at tail
+        if (tail - headCache >= buffer.length) {
+            headCache = head;
+            if (tail - headCache >= buffer.length){
+                //no space
+                return false;
+            }
+        }
+
+        int slot = (int) (tail & mask);
+        buffer[slot] = e;
+        tail++;
+
+        return true;
+    }
+
+    public E poll() {
+        if (tailCache <= head) {
+            tailCache = tail; // force a volatile read
+            if (tailCache <= head) {
+                //no space
+                return null;
+            }
+        } 
+
+        int slot = (int) (head & mask);
+        E val = buffer[slot];
+        head++;
+        return val;
+    }
+
+    public E remove() {
+        final E e = poll();
+        if (null == e) {
+            throw new IllegalStateException("Queue is empty");
+        }
+
+        return e;
+    }
+
+    public E element() {
+        final E e = peek();
+        if (null == e) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        return e;
+    }
+
+    public E peek() {
+        return buffer[(int) (head % buffer.length)];
+    }
+
+    public int size() {
+        long currentHeadBefore;
+        long currentTail;
+        long currentHeadAfter = head;
+
+        do {
+            currentHeadBefore = currentHeadAfter;
+            currentTail = tail;
+            currentHeadAfter = head;
+
+        }
+        while (currentHeadAfter != currentHeadBefore);
+
+        return (int) (currentTail - currentHeadAfter);
+    }
+
+    public boolean isEmpty() {
+        return tail == head;
+    }
+
+    public boolean contains(final Object o) {
+        if (null == o) {
+            return false;
+        }
+
+        for (long i = head, limit = tail; i < limit; i++) {
+            final E e = buffer[(int) (i % buffer.length)];
+            if (o.equals(e)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    public <T> T[] toArray(final T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean remove(final Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean addAll(final Collection<? extends E> c) {
+        for (final E o : c) {
+            add(o);
+        }
+
+        return true;
+    }
+
+    public boolean removeAll(final Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean retainAll(final Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+}
+
